cba093382093355f55fd2bd9d9fca5100d5c551b
phxenix-w <williamnolden98@gmail.com>
2022-01-02 21:08:57 +0100

    finally removed todo from stages

--- game/stages.go
+++ game/stages.go
@@ -1,5 +1,1 @@
 package game
-
-//TODO: -high	make progressive stages, add rare monsters
-//TODO: -low	Figure out a way so that renaming the save file will still delete profiles (on character death for example) (https://golangbyexample.com/change-file-permissions-golang/ maybe)
-//TODO: -low	Use Fighter interface for the damage calculations somehow
